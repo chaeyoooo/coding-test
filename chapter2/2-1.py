989d0d1037d687c0ec8310048be4ff8e7b1be5df
Chaeyeoncho <gcy9293@naver.com>
2024-09-28 18:07:30 +0900

    feat : 파이썬 타입

---
+++ chapter2/2-1.py
@@ -0,0 +1,24 @@
+# 타입 확인
+a = 123
+print(type(a))
+print(a)
+
+b = 123.456
+print(type(b))
+
+# 사칙연산
+a = 7
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) # 나누기
+print(a%b) # 나머지
+print(a//b) # 몫
+print(a**b) # 제곱
+
+# 문자열
+a = 'Hello'
+b = 'World'
+c = '123'
+d = 'I \'m hungry' # 따옴표 안에 따옴표 사용하고 싶을때 \ 사용
