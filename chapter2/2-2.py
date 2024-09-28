ee23c7fe6998d72f688329e97abb251f2177bf76
Chaeyeoncho <gcy9293@naver.com>
2024-09-28 18:07:45 +0900

    feat : 문자열 자료형

---
+++ chapter2/2-2.py
@@ -0,0 +1,12 @@
+# 문자열 연산
+head = "Python"
+tail = " is fun!"
+print(head + tail)
+
+a = "Python"
+print(a * 2) # 문자열 곱하기
+len(a) # 문자열 길이
+
+a = "Life is too short, You need Python"
+print(a[0] + a[1] + a[2] + a[3]) # 문자열 인덱싱 하면 하나의 문자만 가져옴
+print(a[0:4]) # 문자열 슬라이싱 하면 범위만큼 가져옴 [시작 : 끝 : 범위]
