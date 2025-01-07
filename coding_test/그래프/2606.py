8b0efb1ee12c5f83cfff151ae46fd82f37193622
Chaeyeoncho <gcy9293@naver.com>
2025-01-07 15:08:41 +0900

    dfs : 깊이 우선 탐색

---
+++ coding_test/그래프/2606.py
@@ -0,0 +1,20 @@
+n = int(input())
+m = int(input())    
+
+#인접리스트 
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a , b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph , i , visited)
+
+visited = [False] * (n+1)
+
+dfs(graph , 1, visited)
+print(visited.count(True)-1)
