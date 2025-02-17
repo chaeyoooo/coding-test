56cf2c788804d21b160f8f186544c1a1333d3643
조채연 <161800829+Chaeyeoncho@users.noreply.github.com>
2025-02-17 16:00:15 +0900

    [level 2] Title: 주식가격, Time: 151.26 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -11,6 +11,8 @@ def solution(prices):
                 break
         stack.append(count)
     return stack
+
+
 # def solution(prices):
 #     answer = []
 #     for i in range(len(prices)):
@@ -21,6 +23,26 @@ def solution(prices):
 #         for j in range(len(next)):
 #             if prev <= next[j]:
 #                 cnt+=1
+#             else:
+#                 cnt+=1
+#                 break
             
 #         answer.append(cnt)
+#     return answer
+
+
+# def solution(prices):
+#     answer = []
+#     cnt = 0
+#     for i in range(len(prices)): 
+#         cnt = 0
+#         for j in range(i + 1, len(prices)): 
+#             if prices[i] >= prices[j]: 
+#                 cnt += 1 
+#                 break
+#             else:
+#                 cnt += 1
+        
+#         answer.append(cnt) 
+        
 #     return answer
