a1fba5146dbb77756fb2c1732a069096ebe9fa4a
조채연 <161800829+Chaeyeoncho@users.noreply.github.com>
2025-02-21 16:00:45 +0900

    [level 1] Title: 폰켓몬, Time: 1.05 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,8 @@
+def solution(nums):
+    #그니까 nums배열이 있는데 set으로 해서 갯수를 셈 근데 그 갯수가 num //2 보다 크면 num//2 값 출력
+    # 만약 아니면 그냥 set의 갯수를 출력 하면 됨
+    range_nums = set(nums)
+    if len(range_nums) > len(nums) // 2:
+        return len(nums) //2
+    else:
+        return len(range_nums)
