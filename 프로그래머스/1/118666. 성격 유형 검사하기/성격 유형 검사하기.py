a5a0503d0a53d8de91f75c934f14c0fabfdd5248
조채연 <161800829+chaeyoooo@users.noreply.github.com>
2025-03-02 22:05:53 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -28,40 +28,72 @@
 #         answer +="N"
 #     else:
 #         answer += "A"
+
+
+# def solution(survey, choices):
+#     score_map = {1: 3, 2: 2, 3: 1, 4: 0, 5: 1, 6: 2, 7: 3}
+#     scores = {"R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0}
+    
+#     for i in range(len(survey)):
+#         first, second = survey[i][0], survey[i][1]
+#         choice = choices[i]
+#         if choice < 4:
+#             # 비동의 계열
+#             scores[first] += score_map[choice]
+#         elif choice > 4:
+#             # 동의 계열
+#             scores[second] += score_map[choice]
+#         # choice가 4면 아무 점수도 추가하지 않음
+
+#     answer = ""
+#     if scores["R"] >= scores["T"]:
+#         answer += "R"
+#     else:
+#         answer += "T"
+#     if scores["C"] >= scores["F"]:
+#         answer += "C"
+#     else:
+#         answer += "F"
+#     if scores["J"] >= scores["M"]:
+#         answer += "J"
+#     else:
+#         answer += "M"
+#     if scores["A"] >= scores["N"]:
+#         answer += "A"
+#     else:
+#         answer += "N"
+    
+#     return answer
+
 def solution(survey, choices):
-    score_map = {1: 3, 2: 2, 3: 1, 4: 0, 5: 1, 6: 2, 7: 3}
-    scores = {"R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0}
-    
-    for i in range(len(survey)):
-        first, second = survey[i][0], survey[i][1]
-        choice = choices[i]
-        if choice < 4:
-            # 비동의 계열
-            scores[first] += score_map[choice]
-        elif choice > 4:
-            # 동의 계열
-            scores[second] += score_map[choice]
-        # choice가 4면 아무 점수도 추가하지 않음
+    scores = {"A":0, "N":0, "C":0, "F":0, "M":0, "J":0, "R":0, "T":0}
+    for idx, choice in enumerate(choices):
+        if choice - 4 > 0:
+            scores[survey[idx][1]] += choice - 4
+        elif choice - 4 < 0:
+            scores[survey[idx][0]] += 4 - choice
 
-    answer = ""
+    type = ""
     if scores["R"] >= scores["T"]:
-        answer += "R"
+        type += "R"
     else:
-        answer += "T"
-    if scores["C"] >= scores["F"]:
-        answer += "C"
-    else:
-        answer += "F"
-    if scores["J"] >= scores["M"]:
-        answer += "J"
-    else:
-        answer += "M"
-    if scores["A"] >= scores["N"]:
-        answer += "A"
-    else:
-        answer += "N"
-    
-    return answer
+        type += "T"
 
+    if scores["C"] >= scores["F"]:
+        type += "C"
+    else:
+        type += "F"
+
+    if scores["J"] >= scores["M"]:
+        type += "J"
+    else:
+        type += "M"
+
+    if scores["A"] >= scores["N"]:
+        type += "A"
+    else:
+        type += "N"
+
+    return type
         
                 
