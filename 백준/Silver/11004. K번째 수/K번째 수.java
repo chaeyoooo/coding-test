aea6d7adb50728f57f1e67ca0ecbea07c9e0d1f6
조채연 <161800829+chaeyoooo@users.noreply.github.com>
2025-11-17 21:40:18 +0900

    [Silver V] Title: K번째 수, Time: 2156 ms, Memory: 378844 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,29 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 5백만 -> 따라서 O(nlogn) 혹은 O(n) 가능
+    // Scanner로 하면 정수 5백만 개 읽는 것 (2-3초) -> bufferedreader
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[K-1]);
+    }
+}
